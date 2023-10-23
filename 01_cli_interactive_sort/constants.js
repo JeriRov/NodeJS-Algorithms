e9a233c8d12760bad34d31dbafc3d370c655f8b2
Artem Mahey <artemmagej@gmail.com>
2023-10-23 14:26:57 +0300

    feat: cli-interactive-sort done

---
+++ 01_cli_interactive_sort/constants.js
@@ -0,0 +1,18 @@
+module.exports = {
+    COMMAND: {
+        EXIT_COMMAND: "exit",
+    },
+    MESSAGE: {
+        WELCOME_MESSAGE: "Hello. Enter 10 words or digits dividing them in spaces:\n",
+        SORT_TYPE_PROMPT: `How would you like to sort values?
+1. Words by name (A-Z).
+2. Show digits from the smallest.
+3. Show digits from the biggest.
+4. Words by quantity of letters.
+5. Only unique words.
+6. Only unique values from original string.
+Select (1-6) and press ENTER:\n`,
+        GOODBYE_MESSAGE: "Good bye! Come back again!",
+        WRONG_COMMAND: "Wrong command. Try again.",
+    }
+};
